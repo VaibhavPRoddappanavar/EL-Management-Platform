9da9febb8d266b813fcf869faa0918a0d2ac0c1c
VaibhavPRoddappanavar <152184767+VaibhavPRoddappanavar@users.noreply.github.com>
2024-10-27 23:08:19 +0530

    efdf

--- server/server.js
+++ server/server.js
@@ -35,33 +35,47 @@ app.use("/admin", adminRoutes); // Connect admin routes
 
 connectDB();
 
-// Test route to add/post a team
-app.post("/api/teams", async (req, res) => {
-  const { name, email, teamId, role, branch } = req.body;
+//Test route to add/post a team
+app.post('/api/teams', async (req, res) => {
+    const { name, email, teamId, role, branch } = req.body;
 
-  const newTeam = new Team({
-    name,
-    email,
-    teamId,
-    role,
-    branch,
-  });
+    const newTeam = new Team({
+        name,
+        email,
+        teamId,
+        role,
+        branch
+    });
 
-  try {
-    const savedTeam = await newTeam.save(); // Save to MongoDB
-    res.status(201).json(savedTeam);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+    try {
+        const savedTeam = await newTeam.save(); // Save to MongoDB
+        res.status(201).json(savedTeam);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
-//route to get all teams
-app.get("/api/teams", async (req, res) => {
-  try {
-    const teams = await Team.find();
-    res.json(teams);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+
+
+// // Route to insert excel to json converted data
+// app.post('/add-sample-data', async (req, res) => {
+//     try {
+//         const sampleData = req.body; // The sample data will come from the request body
+//         const result = await Team.insertMany(sampleData.Sheet1); // Insert multiple documents
+//         res.status(201).json({ message: 'Data inserted successfully', data: result });
+//     } catch (error) {
+//         res.status(500).json({ message: 'Error inserting data', error: error.message });
+//     }
+// });
+
+
+// Test route to get all teams
+app.get('/api/teams', async (req, res) => {
+    try {
+        const teams = await Team.find(); // Fetch all teams from the database
+        res.json(teams);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 app.get("/", (req, res) => {
