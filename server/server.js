f0e5e28cb03026c34ad3eb51ce0b8f8ce53038ea
chethu3 <chethohaarjm@gmail.com>
2024-10-31 12:46:20 +0530

    hii

--- server/server.js
+++ server/server.js
@@ -36,25 +36,24 @@ app.use("/admin", adminRoutes); // Connect admin routes
 connectDB();
 
 //Test route to add/post a team
-app.post('/api/teams', async (req, res) => {
-    const { name, email, teamId, role, branch } = req.body;
+app.post("/api/teams", async (req, res) => {
+  const { name, email, teamId, role, branch } = req.body;
 
-    const newTeam = new Team({
-        name,
-        email,
-        teamId,
-        role,
-        branch
-    });
+  const newTeam = new Team({
+    name,
+    email,
+    teamId,
+    role,
+    branch,
+  });
 
-    try {
-        const savedTeam = await newTeam.save(); // Save to MongoDB
-        res.status(201).json(savedTeam);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  try {
+    const savedTeam = await newTeam.save(); // Save to MongoDB
+    res.status(201).json(savedTeam);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
-
 
 // // Route to insert excel to json converted data
 // app.post('/add-sample-data', async (req, res) => {
@@ -67,15 +66,14 @@ app.post('/api/teams', async (req, res) => {
 //     }
 // });
 
-
 // Test route to get all teams
-app.get('/api/teams', async (req, res) => {
-    try {
-        const teams = await Team.find(); // Fetch all teams from the database
-        res.json(teams);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+app.get("/api/teams", async (req, res) => {
+  try {
+    const teams = await Team.find(); // Fetch all teams from the database
+    res.json(teams);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.get("/", (req, res) => {
@@ -85,3 +83,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+//hi vaibhav
