eadbd2f0d829c15cd0125eb7407d4a68b73d635f
chethu3 <chethohaarjm@gmail.com>
2024-10-31 12:49:11 +0530

    check

--- server/server.js
+++ server/server.js
@@ -81,7 +81,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running in port ${PORT}`);
 });
 
 //hi vaibhav
